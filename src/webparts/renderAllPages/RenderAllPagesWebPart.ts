075cf5e30e2d468968d55e947320ca0e92ddd44f
marcocrupi <marcocrupi@hotmail.it>
2023-02-09 16:57:06 +0100

    add get request all pages

--- src/webparts/renderAllPages/RenderAllPagesWebPart.ts
+++ src/webparts/renderAllPages/RenderAllPagesWebPart.ts
@@ -1,29 +1,53 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 
-import * as strings from 'RenderAllPagesWebPartStrings';
-import RenderAllPages from './components/RenderAllPages';
-import { IRenderAllPagesProps } from './components/IRenderAllPagesProps';
+import * as strings from "RenderAllPagesWebPartStrings";
+import RenderAllPages from "./components/RenderAllPages";
+import { IRenderAllPagesProps } from "./components/IRenderAllPagesProps";
+import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 
 export interface IRenderAllPagesWebPartProps {
   description: string;
 }
 
-export default class RenderAllPagesWebPart extends BaseClientSideWebPart<IRenderAllPagesWebPartProps> {
+export interface ISPLists {
+  value: ISPList[];
+}
 
-  public render(): void {
-    const element: React.ReactElement<IRenderAllPagesProps> = React.createElement(
-      RenderAllPages,
-      {
-        description: this.properties.description
-      }
-    );
+export interface ISPList {
+  Title: string;
+  Id: string;
+}
+
+export default class RenderAllPagesWebPart extends BaseClientSideWebPart<IRenderAllPagesWebPartProps> {
+  private _getSitePages(): Promise<ISPLists> {
+    return this.context.spHttpClient
+      .get(
+        `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('Site%20Pages')/items`,
+        SPHttpClient.configurations.v1
+      )
+      .then((response: SPHttpClientResponse) => {
+        return response.json();
+      })
+      .catch(() => {
+        return Promise.resolve({ value: [] });
+      });
+  }
+
+  public async render(): Promise<void> {
+    const pagesList: ISPLists = await this._getSitePages();
+    console.log("dataList PAGES", pagesList);
+
+    const element: React.ReactElement<IRenderAllPagesProps> =
+      React.createElement(RenderAllPages, {
+        description: this.properties.description,
+      });
 
     ReactDom.render(element, this.domElement);
   }
@@ -33,7 +57,7 @@ export default class RenderAllPagesWebPart extends BaseClientSideWebPart<IRender
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -41,20 +65,20 @@ export default class RenderAllPagesWebPart extends BaseClientSideWebPart<IRender
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
