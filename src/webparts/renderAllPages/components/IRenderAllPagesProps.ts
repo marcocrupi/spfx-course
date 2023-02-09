141fa233576dcf8f7f5cd47dea65354d99dfabdc
marcocrupi <marcocrupi@hotmail.it>
2023-02-09 16:19:33 +0100

    add render all pages

---
+++ src/webparts/renderAllPages/components/IRenderAllPagesProps.ts
@@ -0,0 +1,3 @@
+export interface IRenderAllPagesProps {
+  description: string;
+}
---
+++ src/webparts/renderAllPages/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IRenderAllPagesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'RenderAllPagesWebPartStrings' {
+  const strings: IRenderAllPagesWebPartStrings;
+  export = strings;
+}
