4f7728bcfdf71c81363a8d0435430b431cd5b62a
marcocrupi <marcocrupi@hotmail.it>
2023-02-09 18:35:28 +0100

    list all pages

--- src/webparts/renderAllPages/components/RenderAllPages.tsx
+++ src/webparts/renderAllPages/components/RenderAllPages.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import styles from "./RenderAllPages.module.scss";
 import { IRenderAllPagesProps } from "./IRenderAllPagesProps";
 import { escape } from "@microsoft/sp-lodash-subset";
-import * as moment from "moment";
 
 export interface ISPList {
   Title: string;
   Description: string;
   Id: string;
   Created: string;
+  BannerImageUrl: any;
 }
 
 export default class RenderAllPages extends React.Component<
@@ -25,17 +25,14 @@ export default class RenderAllPages extends React.Component<
               <h1 className={styles.title}>ALL PAGES</h1>
 
               <div>
-                <div>
+                <div className={styles.allPages}>
                   {allPages.value.map((item: ISPList, index: number) => (
-                    <div key={index}>
-                      <img
-                        src={
-                          "https://miro.medium.com/max/880/0*cesFJY5JFpI0Rl4v.jpg"
-                        }
-                        alt={item.Title}
-                      />
+                    <div key={index} className={styles.boxPage}>
+                      <img src={item.BannerImageUrl.Url} alt={item.Title} />
                       <h2>{item.Title}</h2>
-                      <p>Data di creazione: {item.Created}</p>
+                      <p>
+                        <strong>Data di creazione:</strong> {item.Created}
+                      </p>
                     </div>
                   ))}
                 </div>
